0da3e8926f0a97b8b103347f024c085b020e5be4
hope-coder <1765463091@qq.com>
2022-06-16 15:47:06 +0800

    first

--- data_shap.py
+++ data_shap.py
@@ -6,94 +6,37 @@
 # @File    : data_shap.py
 import xgboost
 import shap
-import random
-import numpy as np
+
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 
-
-def gen_y(X):
-    y = []
-    for i in range(X.shape[0]):
-        x = X.iloc[i,]
-        if x[0] + x[1] > 7:
-            y.append(False)
-        else:
-            y.append(True)
-    return y
-
-
-def normal_data():
-    f1_1 = [random.uniform(0, 5) for _ in range(90)]
-    f1_2 = [random.uniform(5, 10) for _ in range(810)]
-    f2 = [random.uniform(0, 10) for _ in range(900)]
-    f3 = [random.uniform(0, 10) for _ in range(900)]
-    f1 = f1_2 + f1_1
-    random.shuffle(f1)
-    tmp = list(zip(f1, f2, f3))
-    x = [list(i) for i in tmp]
-    df = pd.DataFrame(x)
-    y = gen_y(df)
-    df['y'] = y
-    return df
-
-
-def drift_data():
-    drift_f1_1 = [random.uniform(0, 5) for _ in range(90)]
-    drift_f1_2 = [random.uniform(5, 10) for _ in range(10)]
-    drift_f2 = [random.uniform(0, 10) for _ in range(100)]
-    drift_f3_1 = [random.uniform(0, 5) for _ in range(10)]
-    drift_f3_2 = [random.uniform(5, 10) for _ in range(90)]
-    drift_f1 = drift_f1_2 + drift_f1_1
-    random.shuffle(drift_f1)
-    drift_f3 = drift_f3_2 + drift_f3_1
-    random.shuffle(drift_f3)
-    drift_temp = list(zip(drift_f1, drift_f2, drift_f3))
-    drift_x = [list(i) for i in drift_temp]
-    drift_Data = pd.DataFrame(drift_x)
-    drift_y = gen_y(drift_Data)
-    drift_Data['y'] = drift_y
-    return drift_Data
+import dataset_generate
 
 
 class object_model:
-    def __init__(self, windows_size, test=0.2, type="SEA", classification=True, shap_class=0):
+    def __init__(self, windows_size, test=0.2, dataset="SEA", classification=True, shap_class=0):
         shap.initjs()  # notebook环境下，加载用于可视化的JS代码
         self.classification = classification
         # 我们先训练好一个XGBoost model
         self.windows_size = windows_size
         self.shap_class = shap_class
-        if type == "XGBoost":
-            X, y = shap.datasets.boston()
-            data_size = X.shape[0]
-            train_size = int(data_size * (1 - test))
-            self.X_train = X[:train_size]
-            self.X_test = X[train_size:]
-            self.y_train = y[:train_size]
-            self.y_test = y[train_size:]
-            test_size = data_size - train_size
-            if windows_size * 2 >= test_size:
-                raise Exception("窗口太大")
-            model = xgboost.train({"learning_rate": 0.01}, xgboost.DMatrix(self.X_train, label=self.y_train), 100)
-            self.explainer = shap.TreeExplainer(model)
-        else:
-            train_data = normal_data()
-            drift = drift_data()
-            data = pd.concat([train_data, drift], axis=0, ignore_index=True)
-            X = data.iloc[:, 0:-1]
-            y = data.iloc[:, -1]
-            data_size = X.shape[0]
-            train_size = int(data_size * (1 - test))
-            self.X_train = X[:train_size]
-            self.X_test = X[train_size:]
-            self.y_train = y[:train_size]
-            self.y_test = y[train_size:]
-            test_size = data_size - train_size
-            if windows_size * 2 > test_size:
-                raise Exception("窗口太大")
+        X, y = dataset_generate.getDateset(dataset)
+        data_size = X.shape[0]
+        train_size = int(data_size * (1 - test))
+        self.X_train = X[:train_size]
+        self.X_test = X[train_size:]
+        self.y_train = y[:train_size]
+        self.y_test = y[train_size:]
+        test_size = data_size - train_size
+        if windows_size * 2 >= test_size:
+            raise Exception("窗口太大")
+        if dataset == "SEA":
             log_reg = RandomForestClassifier(n_estimators=20)
             log_reg.fit(self.X_train, self.y_train)
             self.explainer = shap.KernelExplainer(log_reg.predict_proba, self.X_train)
+        elif dataset == "XGBoost":
+            model = xgboost.train({"learning_rate": 0.01}, xgboost.DMatrix(self.X_train, label=self.y_train), 100)
+            self.explainer = shap.TreeExplainer(model)
 
     def getRefWindows(self):
         X_ref = self.X_test[:self.windows_size]
---
+++ dataset_generate.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2022/6/16 15:30
+# @Author  : ZWP
+# @Desc    : 
+# @File    : dataset_generate.py
+
+import random
+import pandas as pd
+import shap
+from river import synth
+
+
+def getDateset(dataset="SEA", num=1000):
+    if dataset == "SEA":
+        train_data = normal_data()
+        drift = drift_data()
+        data = pd.concat([train_data, drift], axis=0, ignore_index=True)
+        X = data.iloc[:, 0:-1]
+        y = data.iloc[:, -1]
+        return X, y
+    elif dataset == "XGBoost":
+        X, y = shap.datasets.boston()
+        return X, y
+    else:
+        data = synth.RandomRBFDrift(seed_model=42, seed_sample=42, n_classes=4, n_features=4, n_centroids=20,
+                                       change_speed=0.87, n_drift_centroids=10)
+        for x, y in data.take(num):
+            print(x, y)
+
+def gen_y(X):
+    y = []
+    for i in range(X.shape[0]):
+        x = X.iloc[i,]
+        if x[0] + x[1] > 7:
+            y.append(False)
+        else:
+            y.append(True)
+    return y
+
+
+def normal_data():
+    f1_1 = [random.uniform(0, 5) for _ in range(90)]
+    f1_2 = [random.uniform(5, 10) for _ in range(810)]
+    f2 = [random.uniform(0, 10) for _ in range(900)]
+    f3 = [random.uniform(0, 10) for _ in range(900)]
+    f1 = f1_2 + f1_1
+    random.shuffle(f1)
+    tmp = list(zip(f1, f2, f3))
+    x = [list(i) for i in tmp]
+    df = pd.DataFrame(x)
+    y = gen_y(df)
+    df['y'] = y
+    return df
+
+
+def drift_data():
+    drift_f1_1 = [random.uniform(0, 5) for _ in range(90)]
+    drift_f1_2 = [random.uniform(5, 10) for _ in range(10)]
+    drift_f2 = [random.uniform(0, 10) for _ in range(100)]
+    drift_f3_1 = [random.uniform(0, 5) for _ in range(10)]
+    drift_f3_2 = [random.uniform(5, 10) for _ in range(90)]
+    drift_f1 = drift_f1_2 + drift_f1_1
+    random.shuffle(drift_f1)
+    drift_f3 = drift_f3_2 + drift_f3_1
+    random.shuffle(drift_f3)
+    drift_temp = list(zip(drift_f1, drift_f2, drift_f3))
+    drift_x = [list(i) for i in drift_temp]
+    drift_Data = pd.DataFrame(drift_x)
+    drift_y = gen_y(drift_Data)
+    drift_Data['y'] = drift_y
+    return drift_Data
